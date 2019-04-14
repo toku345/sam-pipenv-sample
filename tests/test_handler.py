74d8fab5565a12b094c0ff10a51a0f8c622bb70e
Fumitaka Tokumitsu <toku345@gmail.com>
2019-04-14 16:27:33 +0900

    Refactor test

--- tests/test_handler.py
+++ tests/test_handler.py
@@ -1,7 +1,5 @@
 import json
 
-# import pytest
-
 from proverb.app import lambda_handler
 
 
@@ -12,4 +10,3 @@ def test_lambda_handler():
     assert ret["statusCode"] == 200
     assert "message" in ret["body"]
     assert data["message"] == "hello world"
-    # assert "location" in data.dict_keys()
